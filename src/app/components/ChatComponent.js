b08d1297a25a9ca2e6b812193a0c54d744db8937
sayyad-moin-ali <alisayyadmoin@gmail.com>
2025-01-21 10:35:15 +0530

    integrated paragraph

--- src/app/components/ChatComponent.js
+++ src/app/components/ChatComponent.js
@@ -8,7 +8,9 @@ import MessageParser from '../MessageParser'
 
 const ChatComponent = () => {
   return (
-    <div className='bg-slate-600  flex  justify-center items-center h-svh'>
+    <div className='bg-slate-600  flex flex-col  justify-center items-center h-svh'>
+      <div className='bg-white text-lg text-black px-5 py-1 mb-2 rounded-md lg:w-[70%] w-[85%]'><h1>
+      TripNavigator is your smart travel buddy that helps you find the best destinations, plan trips, and get weather updates. It even translates phrases to make your travels easier and more enjoyable!</h1></div>
       <Chatbot
       config={config}
       actionProvider={ActionProvider}
